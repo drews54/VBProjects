fcecc77969e38bdcf2868d037869eab5fab82fb4
Амосов Андрей <duha9898@gmail.com>
2017-04-18 00:01:19 +0300

    Ex. 1.1

--- Week7/Part1/frmSeasons.vb
+++ Week7/Part1/frmSeasons.vb
@@ -1,3 +1,15 @@
 ﻿Public Class frmSeasons
+    Private Sub cmdMS_Click(sender As Object, e As EventArgs) Handles cmdMS.Click
+        Dim intMonth As Integer, blnMonth As Boolean, strSeason As String
 
+        blnMonth = IsNumeric(txtMonth)
+        If blnMonth Then
+            strSeason = "The season is "
+        Else
+            strSeason ="Error"
+            MsgBox("Input a correct number",
+                   vbCritical,
+                   "Input Error")
+        End If
+    End Sub
 End Class
