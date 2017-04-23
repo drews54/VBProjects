4172417e8dd29c0080389a82d4c53b4e4ec5d863
Амосов Андрей <duha9898@gmail.com>
2017-04-23 19:49:10 +0300

    Ex. 1.2

--- Week9/prjProgr/frmProgr.vb
+++ Week9/prjProgr/frmProgr.vb
@@ -2,16 +2,16 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim a%, sum%, strProgr$
         a = 1 : sum = 0 : strProgr = vbEmpty
-        Do
+        Do While a <= 5
             sum += a
             strProgr +=
                 "a = " + CStr(a) + Space(6) +
                 "sum = " + CStr(sum) + vbCrLf
-            TextBox1.Text = strProgr
             a += 1
-            If a > 5 Then
-                Exit Do
-            End If
+            'If a > 5 Then
+            '    Exit Do
+            'End If
         Loop
+        TextBox1.Text = strProgr
     End Sub
 End Class
