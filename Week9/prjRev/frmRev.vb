bc9a55c029ca7672aafd8c0723422f146ad7896b
Амосов Андрей <duha9898@gmail.com>
2017-04-23 21:16:27 +0300

    Ex. 2.1

---
+++ Week9/prjRev/frmRev.vb
@@ -0,0 +1,12 @@
+﻿Option Explicit On
+
+Public Class frmRev
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Long, strT2 As String, digit As Byte
+        If Not IsNumeric(TextBox1.Text) Then
+            TextBox2.Text = "Error"
+            MsgBox("Input a number", vbCritical, "Input Error")
+            Exit Sub
+        End If
+    End Sub
+End Class
