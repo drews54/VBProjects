d2b0f62329851d4b328139a0209d9c333fc606d0
Амосов Андрей <duha9898@gmail.com>
2017-05-05 21:39:47 +0300

    Ex. 1.4 (clarified array indices' issue in code comments)

--- Week10/ArrayInit/frmArrayInit.vb
+++ Week10/ArrayInit/frmArrayInit.vb
@@ -1,7 +1,9 @@
-﻿Public Class frmArrayInit
+﻿'Option Base 1 'Appears to be excluded from VB.NET - thanks CLS!
+
+Public Class frmArrayInit
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'Dim A(1 To 5) As String 'Apparently you cannot declare arrays beginning with anything but 0 - thanks CLS!
-        Dim A(4) As String 'This is a 0 to 4 array of strings
+        'Dim A(1 To 5) As String 'Apparently you cannot declare arrays beginning with anything but 0. Once again - thanks CLS!
+        Dim A(0 To 4) As String 'This is a 0 to 4 array of strings - we will have to deal with that.
         Dim i As Integer, strA As String
         For i = 0 To 4
             A(i) = InputBox($"Input name({i})", "Students' names initialization")
@@ -12,5 +14,20 @@
             strA += A(i) + vbCrLf
         Next i
         TextBox1.Text = strA
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim A(4) As String
+        Dim i%, strA$
+        A(0) = "Spoilers"
+        A(1) = "Flaps"
+        A(2) = "Slats"
+        A(3) = "Ailerons"
+        A(4) = "Winglets"
+        strA = String.Empty
+        For i = 0 To 4
+            strA += A(i) + vbCrLf
+        Next i
+        TextBox2.Text = strA
     End Sub
 End Class
