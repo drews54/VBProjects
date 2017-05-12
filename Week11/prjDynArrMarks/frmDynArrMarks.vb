4e7e42a4cb026326c1dd557342ca458d66a10d36
Амосов Андрей <duha9898@gmail.com>
2017-05-12 23:04:35 +0300

    Ex. 2.2

--- Week11/prjDynArrMarks/frmDynArrMarks.vb
+++ Week11/prjDynArrMarks/frmDynArrMarks.vb
@@ -1,3 +1,13 @@
 ﻿Public Class frmDynArrMarks
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim student$(), n%, s$, mark%()
 
+        n = -1
+        Do
+            n += 1
+            s = InputBox($"Input Name({n + 1})", "Students' Names Input")
+            ReDim Preserve student(n)
+            student(n) = s
+        Loop Until s = String.Empty
+    End Sub
 End Class
