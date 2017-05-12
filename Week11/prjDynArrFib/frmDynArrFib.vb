2d192b710d5ef6ec579d0a132bdf28748d147ea4
Амосов Андрей <duha9898@gmail.com>
2017-05-12 21:40:59 +0300

    Ex. 1.2

--- Week11/prjDynArrFib/frmDynArrFib.vb
+++ Week11/prjDynArrFib/frmDynArrFib.vb
@@ -1,3 +1,6 @@
 ﻿Public Class frmDynArrFib
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i%, n%, gr1!, gr2!
+        Dim F#()
+    End Sub
 End Class
