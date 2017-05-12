972cdb318222c7ddcaee7546b6659f5e826de2fb
Амосов Андрей <duha9898@gmail.com>
2017-05-12 21:41:58 +0300

    Ex. 1.3

--- Week11/prjDynArrFib/frmDynArrFib.vb
+++ Week11/prjDynArrFib/frmDynArrFib.vb
@@ -2,5 +2,12 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim i%, n%, gr1!, gr2!
         Dim F#()
+
+        If Not IsNumeric(TextBox1.Text) Then
+            TextBox2.Clear() : TextBox3.Clear() : TextBox4.Clear()
+            MsgBox("Input a nonnegative integer number", vbCritical, "Input Error")
+            TextBox1.Select()
+            Exit Sub
+        End If
     End Sub
 End Class
